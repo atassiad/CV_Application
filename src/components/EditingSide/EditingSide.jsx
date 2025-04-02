f8bfe7e7eeb3a26a9861c63b814fc01ca9c44d50
atassiad <atassiadamis@gmail.com>
2025-04-01 22:47:19 -0700

    Added some image assets, improved the UI and will begin working on the input forms whose outputs will connect to the resume side

--- src/components/EditingSide/EditingSide.jsx
+++ src/components/EditingSide/EditingSide.jsx
@@ -1,6 +1,52 @@
 import React from 'react'
 import './EditingSide.css'
+import generalIcon from "/home/athanasios/Odin/CV_Application/src/assets/business-man-wearing-suit-and-tie-svgrepo-com.svg";
+import educationIcon from "/home/athanasios/Odin/CV_Application/src/assets/school-svgrepo-com.svg";
+import workIcon from "/home/athanasios/Odin/CV_Application/src/assets/work-alt-svgrepo-com.svg";
 
+function General() {
+    return (
+        <div className="general">
+            <div className="top">
+                <img src={generalIcon}></img>
+                <p> General Information </p>
+            </div>
+            <div className="bottom">
+                <form className="general-input">
+                    
+                </form>
+            </div>
+        </div>
+    );
+}
+
+function Education() {
+    return (
+        <div className="education">
+            <div className="top">
+                <img src={educationIcon}></img>
+                <p> Education </p>
+            </div>
+            <div className="bottom">
+
+            </div>
+        </div>
+    );
+}
+
+function WorkExperience() {
+    return (
+        <div className="work-experience"> 
+            <div className="top">
+                <img src={workIcon}></img>
+                <p> Work </p>
+            </div>
+            <div className="bottom">
+
+            </div>
+        </div>
+    );
+}
 function EditingSide() {
     return (
         <div className="editing-side">
@@ -8,6 +54,9 @@ function EditingSide() {
                 <h1> Resume Editor </h1>
             </div>
             <div className="testing">
+                <General/>
+                <Education/>
+                <WorkExperience/>
             </div>
         </div>
     )
